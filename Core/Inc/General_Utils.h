fda24d0ba8763e70c499bfb477c77c721b7e1420
ZacharyTarkowski <66281211+ZacharyTarkowski@users.noreply.github.com>
2024-11-27 15:57:38 -0800

    initial commit

---
+++ Core/Inc/General_Utils.h
@@ -0,0 +1,45 @@
+/*
+ * UART_Utils.h
+ *
+ *  Created on: Aug 13, 2024
+ *      Author: zacht
+ */
+
+#ifndef SRC_GENERAL_UTILS_H_
+#define SRC_GENERAL_UTILS_H_
+
+#include "usart.h"
+#include "string.h"
+#include "stdio.h"
+#include "stdarg.h"
+
+extern UART_HandleTypeDef huart2;
+
+
+typedef enum {
+    ADD,
+    SUB,
+    MULTIPLY,
+    DIVIDE,
+}operation_e;
+
+
+#define u8 uint8_t
+#define u16 uint16_t
+#define u32 uint32_t
+
+#define s8  int8_t
+#define s16 int16_t
+#define s32 int32_t
+
+typedef s32 dtw_seq_t;
+
+#define BAD_PRINT_STRING "Bad Print Call\n\r"
+
+void printstring(char* msg);
+
+void uart_printf(const char* fmt, ...);
+
+void uart_println(const char* fmt, ...);
+
+#endif /* SRC_GENERAL_UTILS_H_ */
