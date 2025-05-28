fcea9b37031ba06e15a9f03c02594f2d14d3949d
ZacharyTarkowski <zach.tarkowski@gmail.com>
2025-05-27 21:44:29 -0700

    final code fixes to add pir sensor and interrupt

--- Core/Src/Hippo_Logic.c
+++ Core/Src/Hippo_Logic.c
@@ -3,16 +3,16 @@
 
 void run_idle_state()
 {
-    ILI9341_Draw_Run_Length_Encoded_Image((const uint16_t*)hippo_asleep_unfinished, sizeof(hippo_asleep_unfinished)/sizeof(uint16_t) );
+    ILI9341_Draw_Run_Length_Encoded_Image((const uint16_t*)Hippo_Asleep_1, sizeof(Hippo_Asleep_1)/sizeof(uint16_t) );
     HAL_Delay(250);
-    ILI9341_Draw_Run_Length_Encoded_Image((const uint16_t*)hippo_asleep_unfinished, sizeof(hippo_asleep_unfinished)/sizeof(uint16_t) );
+    ILI9341_Draw_Run_Length_Encoded_Image((const uint16_t*)Hippo_Asleep_2, sizeof(Hippo_Asleep_2)/sizeof(uint16_t) );
     HAL_Delay(250);
 }
 
 void run_active_state()
 {
-    ILI9341_Draw_Run_Length_Encoded_Image((const uint16_t*)hippo_awake_unfinished, sizeof(hippo_awake_unfinished)/sizeof(uint16_t) );
-    HAL_Delay(250);
-    ILI9341_Draw_Run_Length_Encoded_Image((const uint16_t*)hippo_awake_unfinished, sizeof(hippo_awake_unfinished)/sizeof(uint16_t) );
-    HAL_Delay(250);
+    ILI9341_Draw_Run_Length_Encoded_Image((const uint16_t*)Hippo_Awake_1, sizeof(Hippo_Awake_1)/sizeof(uint16_t) );
+    HAL_Delay(125);
+    ILI9341_Draw_Run_Length_Encoded_Image((const uint16_t*)Hippo_Awake_2, sizeof(Hippo_Awake_2)/sizeof(uint16_t) );
+    HAL_Delay(125);
 }
